0ad9553d21e191fccfa10549be879e5a7925215d
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-05 15:45:17 +0300

    add routing and home page implementation

--- lib/core/helpers/app_router.dart
+++ lib/core/helpers/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:note_flow/constants/my_strings.dart';
-import 'package:note_flow/screens/home_page.dart';
+import 'package:note_flow/core/constants/my_strings.dart';
+import 'package:note_flow/layers/ui/screens/home_page.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
--- lib/layers/ui/screens/home_page.dart
+++ lib/layers/ui/screens/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:note_flow/layers/ui/widgets/my_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: MyAppbar(appBarTitle:'NoteFlow',onpressed:(){}),
+    );
   }
 }
