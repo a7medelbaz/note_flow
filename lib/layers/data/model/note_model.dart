bff816ecc56434effa78dc494775502300c29209
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-08 20:54:01 +0300

    create note model

---
+++ lib/layers/data/model/note_model.dart
@@ -0,0 +1,16 @@
+class NoteModel {
+  final int id;
+  final String title;
+  final String subTitle;
+  final DateTime dateTime;
+  final int color;
+
+  NoteModel({
+    required this.id,
+    required this.title,
+    required this.subTitle,
+    required this.dateTime,
+    required this.color,
+  });
+  
+}
