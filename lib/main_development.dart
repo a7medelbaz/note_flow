3b578e8ba9094b9d17cfeabe6e9dca10d627332c
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-08 21:41:08 +0300

    open box in hive NoteBox

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_flow/core/constants/my_strings.dart';
 import 'package:note_flow/core/helpers/app_router.dart';
 import 'package:note_flow/layers/ui/widgets/my_general_widgets.dart';
 
@@ -25,5 +26,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Initialize Hive
   await Hive.initFlutter();
+  await Hive.openBox(MyConstants.myNoteHiveBox);
+  // Hive.registerAdapter(NoteModelAdapter());
   runApp(NoteFlow(appRouter: AppRouter()));
 }
