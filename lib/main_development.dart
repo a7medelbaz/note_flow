ceea323629b7bced95d82200aba824f94ac6b419
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-08 21:00:44 +0300

    feat: integrate Hive and path_provider dependencies, update main function for initialization

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:note_flow/core/helpers/app_router.dart';
 import 'package:note_flow/layers/ui/widgets/my_general_widgets.dart';
 
@@ -20,6 +21,9 @@ class NoteFlow extends StatelessWidget {
   }
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Hive
+  await Hive.initFlutter();
   runApp(NoteFlow(appRouter: AppRouter()));
 }
