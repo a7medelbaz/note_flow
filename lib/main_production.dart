3dcde367d83abfc5330c0c8b705719989f6b8b75
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-09 04:27:31 +0300

    init note model adapter

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_flow/core/constants/my_strings.dart';
 import 'package:note_flow/core/helpers/app_router.dart';
+import 'package:note_flow/layers/data/model/note_model.dart';
 import 'package:note_flow/layers/ui/widgets/my_general_widgets.dart';
 
 class NoteFlow extends StatelessWidget {
@@ -25,5 +27,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Initialize Hive
   await Hive.initFlutter();
+  await Hive.openBox(MyConstants.myNoteHiveBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(NoteFlow(appRouter: AppRouter()));
 }
