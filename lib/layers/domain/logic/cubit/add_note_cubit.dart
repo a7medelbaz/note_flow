f617553a1a69f6c6630138334c7f15b074fc2cde
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-09 04:24:39 +0300

    build AddNote Cubit

---
+++ lib/layers/domain/logic/cubit/add_note_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+part 'add_note_state.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+}
