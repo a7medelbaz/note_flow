3ca9dce0d70e309a4b526ca22184cdb4f6e43b32
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-11 01:44:10 +0300

    fixing hive type

--- lib/layers/domain/logic/cubit/add_note_cubit/add_note_cubit.dart
+++ lib/layers/domain/logic/cubit/add_note_cubit/add_note_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:hive_flutter/adapters.dart';
+
 import '../../../../../core/constants/my_strings.dart';
 import '../../../../data/model/note_model.dart';
 
@@ -23,7 +24,7 @@ class AddNoteCubit extends Cubit<AddNoteState> {
         return; // نوقف التنفيذ هنا
       }
       emit(AddNoteLoading());
-      var notesBox = Hive.box(
+      var notesBox = Hive.box<NoteModel>(
         MyConstants.myNoteHiveBox,
       );
       await notesBox.add(noteModel);
