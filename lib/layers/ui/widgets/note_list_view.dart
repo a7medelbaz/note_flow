3fd29ee60128ed86bcbaf2d19873cbca1cef80d4
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-10-11 01:46:40 +0300

    Implement Bloc pattern in NoteListView for state management

--- lib/layers/ui/widgets/note_list_view.dart
+++ lib/layers/ui/widgets/note_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../domain/logic/cubit/read_note_cubit/notes_cubit.dart';
 
 import 'note_card.dart';
 
@@ -7,15 +9,44 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 1,
-      itemBuilder: (context, index) {
-        return NoteCard(
-          title: ' My Flutter Course',
-          subTitle: 'Hi my name is Ahmed ',
-          deleteFunction: () {},
-          time: DateTime.now(),
-        );
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        if (state is NotesLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is NotesSuccesss) {
+          return ListView.builder(
+            itemCount: state.loadingNotes.length,
+            itemBuilder: (context, index) {
+              return NoteCard(
+                title: state
+                    .loadingNotes[index]
+                    .title,
+                subTitle: state
+                    .loadingNotes[index]
+                    .subTitle,
+                deleteFunction: () {},
+                time: state
+                    .loadingNotes[index]
+                    .dateTime,
+              );
+            },
+          );
+        } else if (state is NotesFailure) {
+          return Center(
+            child: Text(
+              'Error: ${state.errorMassage}',
+              style: const TextStyle(
+                color: Colors.red,
+              ),
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text('No notes yet'),
+          );
+        }
       },
     );
   }
